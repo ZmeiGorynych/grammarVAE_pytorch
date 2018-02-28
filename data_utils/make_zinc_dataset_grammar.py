Fixed error in pytorch Decoder (was relu(sigmoid) instead of softmax), more refactoring of code for grammarVAE

data_utils/make_zinc_dataset_grammar.py
@@ -1,25 +1,22 @@
 from __future__ import print_function
 import nltk
 #import pdb
-import zinc_grammar
+from models import grammar_zinc, zinc_tokenizer
 import numpy as np
 import h5py
-import zinc_tokenizer
-
-
 
 MAX_LEN=277
-NCHARS = len(zinc_grammar.GCFG.productions())
+NCHARS = len(grammar_zinc.GCFG.productions())
 
 def to_one_hot(smiles):
     """ Encode a list of smiles strings to one-hot vectors """
     assert type(smiles) == list
     prod_map = {}
-    for ix, prod in enumerate(zinc_grammar.GCFG.productions()):
+    for ix, prod in enumerate(grammar_zinc.GCFG.productions()):
         prod_map[prod] = ix
-    tokenize = zinc_tokenizer.get_zinc_tokenizer(zinc_grammar.GCFG)
+    tokenize = zinc_tokenizer.get_zinc_tokenizer(grammar_zinc.GCFG)
     tokens = map(tokenize, smiles)
-    parser = nltk.ChartParser(zinc_grammar.GCFG)
+    parser = nltk.ChartParser(grammar_zinc.GCFG)
     parse_trees = [next(parser.parse(t)) for t in tokens]
     productions_seq = [tree.productions() for tree in parse_trees]
     indices = [np.array([prod_map[prod] for prod in entry], dtype=int) for entry in productions_seq]

models/model_grammar_pytorch.py
@@ -36,7 +36,16 @@ class Decoder(nn.Module):
         out_3, hidden_3 = self.gru_3(out_2, hidden_3)
         out = self.fc_out(out_3.contiguous().view(-1, self.hidden_n)).view(_batch_size, self.max_seq_length,
                                                                            self.output_feature_size)
-        return F.relu(F.sigmoid(out)), hidden_1, hidden_2, hidden_3
+        # WTF RELU(sigmoid)?
+        #return F.relu(F.sigmoid(out)), hidden_1, hidden_2, hidden_3
+        return F.softmax(out,2), hidden_1, hidden_2, hidden_3
+
+    def decode(self, z):
+        # TODO: should actually be handling single vectors, not batches, dependent on dim?
+        batch_size = z.size()[0]
+        h1, h2, h3 = self.decoder.init_hidden(batch_size)
+        output, h1, h2, h3 = self.decoder(z, h1, h2, h3)
+        return output.data.numpy()
 
     def init_hidden(self, batch_size):
         # NOTE: assume only 1 layer no bi-direction
@@ -72,8 +81,11 @@ class Encoder(nn.Module):
         h = self.fc_0(x_)
         return self.fc_mu(h), self.fc_var(h)
 
+    def encode(self,x):
+        mu_, var_ = self.forward(x)
+        return mu_.data.numpy(), var_.data.numpy()
 
-from visdom_helper.visdom_helper import Dashboard
+#from visdom_helper.visdom_helper import Dashboard
 
 
 class VAELoss(nn.Module):
@@ -82,7 +94,7 @@ class VAELoss(nn.Module):
         super(VAELoss, self).__init__()
         self.bce_loss = nn.BCELoss()
         self.bce_loss.size_average = False
-        self.dashboard = Dashboard('Variational-Autoencoder-experiment')
+        #self.dashboard = Dashboard('Variational-Autoencoder-experiment')
 
     # question: how is the loss function using the mu and variance?
     def forward(self, x, mu, log_var, recon_x):

run_grammar_vae_2.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 from torch.autograd import Variable
 from torch.optim import lr_scheduler
 
-from models.grammar_vae_model import GrammarVariationalAutoEncoder, VAELoss
+from models.model_grammar_pytorch import GrammarVariationalAutoEncoder, VAELoss
 from visdom_helper.visdom_helper import Dashboard
 from basic_pytorch.fit import fit
 